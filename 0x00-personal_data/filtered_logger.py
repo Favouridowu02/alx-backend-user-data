8d429fbc6f6f9af5bee136e867b64a26e55b2f80
Favour Idowu <favouridowu02@gmail.com>
2024-11-11 19:46:20 +0100

    Add a file filtered_logger.py that contains a function filter_datum
    Implemented a function filter_datum that accepts four Arguments. fields a list of strings representing all fields to obfuscate, redaction: a string representing by what the field will be obfuscated, message: a string representing the log line, separator: a string representing by which character is separating all fields in the log line (message). The function returns the updated verion of the message

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -7,4 +7,19 @@ from typing import List
 import re
 
 
-def filter_datum(fields: List[str], redaction: str, message: str, seperator: str) -> 
+def filter_datum(fields: List[str], redaction: str, message: str,
+                 seperator: str) -> str:
+    """
+        This function returns the log message obfuscated
+
+        Arguments:
+        fields: a list of strings representing all fields to obfuscate
+        redaction: a string representing by what the field will be
+                obfuscated
+        message: a string representing the log line
+        separator: a string representing by which character is separating
+    """
+    for key in fields:
+        message = re.sub(fr'{key}=[^ {seperator}]+', f"{key}={redaction}",
+                         message)
+    return message
