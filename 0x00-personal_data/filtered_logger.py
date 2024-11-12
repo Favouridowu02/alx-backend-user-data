e380840a27f15287b9fb24ca227e981402db7332
Favour Idowu <favouridowu02@gmail.com>
2024-11-12 01:44:59 +0100

    Add a file that contains a class that is used to set up a Logger
    Implemneted a function in the class format. It accepts one argument format(Logging.LogRecord). and retunr a redacted version of the message of the instance

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -5,6 +5,7 @@
 """
 from typing import List
 import re
+import logging
 
 
 def filter_datum(fields: List[str], redaction: str, message: str,
@@ -23,3 +24,24 @@ def filter_datum(fields: List[str], redaction: str, message: str,
         message = re.sub(fr'{key}=[^ {seperator}]+', f"{key}={redaction}",
                          message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+    """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields):
+        """This is the intialization of the class"""
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """This method is used to format the logging"""
+        msg = super().format(record)
+        final_message = filter_datum(self.fields, self.REDACTION, msg,
+                                     self.SEPARATOR)
+        return final_message
