d2b0aa29a2e46300c77434b02578821ce77ff9e1
Favour Idowu <favouridowu02@gmail.com>
2024-11-12 02:10:04 +0100

    Updated the class

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -34,14 +34,14 @@ class RedactingFormatter(logging.Formatter):
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
-    def __init__(self, fields):
+    def __init__(self, fields: List[str]):
         """This is the intialization of the class"""
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
         """This method is used to format the logging"""
-        msg = super().format(record)
+        msg = super(RedactingFormatter, self).format(record)
         final_message = filter_datum(self.fields, self.REDACTION, msg,
                                      self.SEPARATOR)
         return final_message
