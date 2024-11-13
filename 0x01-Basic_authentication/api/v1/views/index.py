9ee751c60ea563d0e9d710836626ae1606aa578c
Favour Idowu <favouridowu02@gmail.com>
2024-11-13 21:20:47 +0100

    Updated the app.py file to handle 403 errors
    Implemented a route 'api/v1/forbidden' that raises a 403 error in the index.py file

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -36,7 +36,7 @@ def unauthorized():
 
 
 @app_views.route("/forbidden", strict_slashes=False)
-def forbidden():
+def forbidden(error):
     """ GET /api/v1/forbidden
     Return:
       - Abort Forbidden
