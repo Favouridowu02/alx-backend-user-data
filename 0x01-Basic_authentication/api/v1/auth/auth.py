3650f871e9bd3c545d09da9720b3b96265ae8c4f
Favour Idowu <favouridowu02@gmail.com>
2024-11-13 23:53:07 +0100

    updated the method require_auth based on some checks

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -12,15 +12,31 @@ class Auth:
         This class is used to manage the API authentication
 
         Methods:
-          - require_auth:
+          - require_auth: This is the require_auth method
           - authorization_header:
           - current_user:
     """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """
-            This method return false
+            This is the require_auth method
+
+            Arguments:
+              - path: the paths
+              - excluded_path: The excluded path
+
+            Return:
+              - True if path is None, True if excluded_paths is None
+                or empty, False if path is in excluded_paths
         """
-        return False
+        if path is None:
+            return True
+        if excluded_paths is None or excluded_paths is []:
+            return True
+        if path[-1] != "/":
+            path = "{}/".format(path)
+        if path in excluded_paths:
+            return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """
