a18e26ab780859ab7fc7e70aa80c6196d84420d9
Favour Idowu <favouridowu02@gmail.com>
2024-11-17 07:03:11 +0100

    Updated the file app.py to make it pycodestyle complaint

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -44,7 +44,7 @@ def before_request() -> str:
     if auth.current_user(request) is None:
         abort(403)
     request.current_user = auth.current_user(request)
-    print("\n\n\n",request.current_user.to_json(),"\n\n\n")
+
 
 @app.errorhandler(401)
 def unauthorized(error) -> str:
@@ -63,4 +63,4 @@ def forbidden(error) -> str:
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
-    app.run(host=host, port=port, debug=True)
+    app.run(host=host, port=port)
