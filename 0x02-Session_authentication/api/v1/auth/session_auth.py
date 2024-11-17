9cebb197103a8eaa643da8f913d5f1736be4c850
Favour Idowu <favouridowu02@gmail.com>
2024-11-17 07:32:10 +0100

    Updated the session_auth module
    Implmented a class attribute user_id_session_id to store the session id
    Implemented an instance method create_session that accepts one parameter user_id(Str), stores the created session in the class attribute and returns the session id

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -3,6 +3,7 @@
     This Module contains the SessionAuth class
 """
 from api.v1.auth.auth import Auth
+from uuid import uuid4
 
 
 class SessionAuth(Auth):
@@ -10,4 +11,16 @@ class SessionAuth(Auth):
         This class inherits from the Auth class and would be updated
         soon
     """
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """
+            This Method creates a session Id for the user_id
+        """
+        if user_id is None:
+            return None
+        if not isinstance(user_id, str):
+            return None
+        session_id = str(uuid4())
+        SessionAuth.user_id_by_session_id[session_id] = user_id
+        return session_id
