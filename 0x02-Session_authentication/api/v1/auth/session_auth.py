2c1d9a02aaee6c713b61a6c77c0d238a9c226f31
Favour Idowu <favouridowu02@gmail.com>
2024-11-17 07:14:53 +0100

    Added a module session_auth that creats a class SessionAuth that inherits from the class Auth and creates an instance of the class.
    Updated the app.py module to handle cases where the AUTH_TYPE module is session_auth

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+    This Module contains the SessionAuth class
+"""
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """
+        This class inherits from the Auth class and would be updated
+        soon
+    """
+    pass
