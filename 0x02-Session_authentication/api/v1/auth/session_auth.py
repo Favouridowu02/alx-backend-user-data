be5473b41ad88c1b1812d039e04bcba7cce9b478
Favour Idowu <favouridowu02@gmail.com>
2024-11-17 07:41:08 +0100

    Updated the class SessionAuth in module session_auth
    Implemented a function user_id_for_session. It accepts one argument session_id(Str). It returns the user_id based on the session id

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -16,6 +16,11 @@ class SessionAuth(Auth):
     def create_session(self, user_id: str = None) -> str:
         """
             This Method creates a session Id for the user_id
+
+            Arguments:
+                user_id: the user id
+
+            Return: returns the created session id
         """
         if user_id is None:
             return None
@@ -24,3 +29,19 @@ class SessionAuth(Auth):
         session_id = str(uuid4())
         SessionAuth.user_id_by_session_id[session_id] = user_id
         return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """
+            This Method is used to return a user id based on the
+            session id
+
+            Arguments:
+                session_id: the session Id
+
+            Return: returns the user_id
+        """
+        if session_id is None:
+            return None
+        if not isinstance(session_id, str):
+            return None
+        return SessionAuth.user_id_by_session_id.get(session_id)
