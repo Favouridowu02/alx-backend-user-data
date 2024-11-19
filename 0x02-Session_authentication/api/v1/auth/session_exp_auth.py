0601c3df1952002ce6222f05b9257d9875a36af0
Favour Idowu <favouridowu02@gmail.com>
2024-11-19 17:33:23 +0100

    --

--- 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
@@ -6,10 +6,10 @@
 from flask import request
 from api.v1.auth.session_auth import SessionAuth
 from os import getenv
-from datetime import datetime
+from datetime import datetime, timedelta
 
 
-class SessionExpAuith(SessionAuth):
+class SessionExpAuth(SessionAuth):
     """
         This Class is used to handle the session expiration
     """
@@ -26,9 +26,30 @@ class SessionExpAuith(SessionAuth):
         """
             This Method is used to handle the creation of sessions
         """
-        session_id = super().create_session(self, user_id)
-        if session_id is None:
+        session_id = super().create_session(user_id)
+        if type(session_id) != str:
             return None
 
-        session_dictionary = {"user_id": user_id,
-                              "created_at": datetime.now()}
+        self.user_id_for_session_id[session_id] = {
+            "user_id": user_id,
+            "created_at": datetime.now()
+        }
+        return session_id
+
+    def user_id_for_session_id(self, session_id=None):
+        """
+            This method is used to handle the get user id from session_id
+        """
+        if type(session_id) != str:
+            return None
+        user_id = self.user_id_by_session_id(session_id)
+        if user_id is None:
+            return None
+        if self.session_duration <= 1:
+            return None
+        if user_id.get('created_at') is None:
+            return None
+        time_span = timedelta(seconds=self.session_duration)
+        if user_id['created_at'] + time_span < datetime.now():
+            return None
+        return user_id['user_id']
