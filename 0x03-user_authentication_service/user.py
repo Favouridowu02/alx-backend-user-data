3f3599d9c847892ef52b5994134e28a1b9536fdf
Favour Idowu <favouridowu02@gmail.com>
2024-11-21 21:02:03 +0100

    Add a module user.py that contains a model User that is used to create a user table

---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+    This Module contains the User Authentication Service
+"""
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+
+engine = create_engine('sqlite:///:memory:', echo=True)
+
+Base = declarative_base()
+
+
+class User(Base):
+    """
+        This class is used to create a user table
+    """
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
