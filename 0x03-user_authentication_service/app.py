c1da1d52f7a340bc7e68120892a9c31247072e3d
Favour Idowu <favouridowu02@gmail.com>
2024-11-22 21:14:13 +0100

    Implemented an api route '/users' thats is used to create a new user if it does not exists

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,7 +2,11 @@
 """
     This module contains the setup for the Flask
 """
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
+
+
+AUTH = Auth()
 
 
 app = Flask(__name__)
@@ -14,6 +18,22 @@ def home_route():
       - Returns a jsonified message
     """
     return jsonify({'message': 'Bienvenue'})
+
+
+@app.route('/users', strict_slashes=False, methods=['POST'])
+def user():
+    """ POST '/users'
+      - Returns a newly created user
+    """
+    try:
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        user = AUTH.register_user(email, password)
+        if user:
+            return jsonify({'email': email, 'message': 'user created'})
+    except ValueError:
+        return jsonify({'message': 'email already registered'}), 400
 
 
 if __name__ == "__main__":
