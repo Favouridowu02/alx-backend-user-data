dc5fd721a6610467a3e8af6e56e470e34c668614
Favour Idowu <favouridowu02@gmail.com>
2024-11-22 17:50:25 +0100

    Updated a the db.py module to implement a method find_user_by

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -63,10 +63,8 @@ class DB:
         try:
             user = self._session.query(User).filter_by(**kwargs).first()
             if user is None:
-                raise NoResultFound("No user found")
+                raise NoResultFound
 
             return user
         except InvalidRequestError as e2:
             raise e2
-
-        return user
