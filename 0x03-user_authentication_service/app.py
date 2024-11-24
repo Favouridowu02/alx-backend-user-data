f3de58fa8d7eddb135fff0a7267db8c0eb658e69
Favour Idowu <favouridowu02@gmail.com>
2024-11-24 08:15:50 +0100

    Implemented the Login feature

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -36,5 +36,21 @@ def user():
         return jsonify({'message': 'email already registered'}), 400
 
 
+@app.route('/sessions', strict_slashes=False, methods=['POST'])
+def login():
+    """ GET '/login'
+      - Return 
+    """
+    try:
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        session_id = AUTH.create_session()
+        if session_id is None:
+            abort(401)
+    except:
+        return abort(401)
+
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port='5000')
